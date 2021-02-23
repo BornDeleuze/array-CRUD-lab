30fac49df6bc8bd713e6091f0b903cd06794bea5
Matt <ehxtwo@gmail.com>
2021-02-23 14:15:16 -0800

    commit tree 0f8760c65eb7a67f79c4165895385213f7988094

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,42 @@
 def create_an_empty_array
-  
+    []
 end
 
 def create_an_array
-  
+    ["Bex", "Rachel", "Chris", "Marin"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    friends = ["Bex", "Rachel", "Chris", "Marin"]
+    friends << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    friends = ["Bex", "Rachel", "Chris", "Marin"]
+    friends.unshift "wow"
 end
 
 def remove_element_from_end_of_array(array)
-  
+    friends = ["Bex", "Rachel", "Chris", "arrays!"]
+    friends.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+    friends = ["wow", "Rachel", "Chris", "arrays!"]
+    friends.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    friends = ["wow", "Rachel", "am", "arrays!"]
+    friends[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+    friends = ["wow", "Rachel", "am", "arrays!"]
+    friends.first
 end
 
 def retrieve_last_element_from_array(array)
-  
+    friends = ["wow", "Rachel", "am", "arrays!"]
+    friends.last
 end
